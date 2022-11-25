090b2870e8d46ae69570ec5a5ddb2415d839624a
Qadriddin <qadrdin81@gmail.com>
2022-11-25 16:17:49 +0500

    updated main file

--- main.go
+++ main.go
@@ -3,32 +3,24 @@ package main
 import (
 	"fmt"
 	"github.com/Qadriddin-dev/goPRJ/controllers"
+	"github.com/Qadriddin-dev/goPRJ/templates"
 	"github.com/Qadriddin-dev/goPRJ/views"
 	"github.com/go-chi/chi/v5"
 	"net/http"
-	"path/filepath"
 )
 
 func main() {
 	r := chi.NewRouter()
 
-	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
-	if err != nil {
-		panic(err)
-	}
+	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml"))
 	r.Get("/", controllers.StaticHandler(tpl))
 
-	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
-	if err != nil {
-		panic(err)
-	}
+	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml"))
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
-	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
-	if err != nil {
-		panic(err)
-	}
+	tpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml"))
 	r.Get("/faq", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "404 NOT FOUND:(", http.StatusNotFound)
 	})
