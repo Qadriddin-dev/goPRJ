a632b00b42e7aa10851cf7b5c148d19d50b2309f
Qadriddin <qadrdin81@gmail.com>
2022-11-25 14:42:24 +0500

    added controllers file

---
+++ controllers/static.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+	"github.com/Qadriddin-dev/goPRJ/views"
+
+	"net/http"
+)
+
+func StaticHandler(tpl views.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
+}
