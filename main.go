e4a617145f51eae5a80e25e666d6857ee21e89a5
Qadriddin <qadrdin81@gmail.com>
2022-12-09 15:34:35 +0500

    updated route

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/Qadriddin-dev/goPRJ/controllers"
+	"github.com/Qadriddin-dev/goPRJ/models"
 	"github.com/Qadriddin-dev/goPRJ/templates"
 	"github.com/Qadriddin-dev/goPRJ/views"
 	"github.com/go-chi/chi/v5"
+	"github.com/gorilla/csrf"
 	"net/http"
 )
 
@@ -19,16 +21,40 @@ func main() {
 	r.Get("/faq", controllers.FAQ(
 		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
-	usersC := controllers.Users{}
+	cfg := models.DefaultPostgresConfig()
+	db, err := models.Open(cfg)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	userService := models.UserService{
+		DB: db,
+	}
+
+	usersC := controllers.Users{
+		UserService: &userService,
+	}
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS,
 		"signup.gohtml", "tailwind.gohtml"))
 
+	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS,
+		"signin.gohtml", "tailwind.gohtml"))
+
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
+	r.Post("/signin", usersC.ProcessSignIn)
+	r.Get("/users/me", usersC.CurrentUser)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 	fmt.Println("Starting the server on :8000...")
-	http.ListenAndServe(":8000", r)
+
+	csrfKey := "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
+	csrfMW := csrf.Protect(
+		[]byte(csrfKey),
+		csrf.Secure(false))
+	http.ListenAndServe(":8000", csrfMW(r))
 }
