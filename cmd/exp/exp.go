5dd45ef978b6ca49bc8d36ae6ad4d1f58ab6e3fa
Qadriddin <qadrdin81@gmail.com>
2022-12-09 15:30:15 +0500

    updated example file

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,20 +1,114 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"github.com/Qadriddin-dev/goPRJ/models"
 )
 
-func Connect() error {
-	return errors.New("connection failed")
-}
-
 func main() {
-	// We know Connect can result in an error because it returns an error
-	err := Connect()
+	cfg := models.DefaultPostgresConfig()
+	db, err := models.Open(cfg)
 	if err != nil {
-		// handle the error or panic if we want
+		panic(err)
 	}
-	numbers := []int{1, 2, 3}
-	fmt.Println(numbers[4])
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected!")
+
+	us := models.UserService{
+		DB: db,
+	}
+
+	user, err := us.Create("odinaevamaryam@gmail.com", "mary123")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(user)
+	//
+	//	_, err = db.Exec(`
+	//	CREATE TABLE IF NOT EXISTS users (
+	//	    id SERIAL PRIMARY KEY,
+	//	    name TEXT,
+	//	    email TEXT UNIQUE NOT NULL
+	//    );
+	//
+	//	CREATE TABLE IF NOT EXISTS orders (
+	//	    id SERIAL PRIMARY KEY,
+	//	    user_id INT NOT NULL,
+	//	    amount INT,
+	//	    description TEXT
+	//	);
+	//`)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//	fmt.Println("Tables created!")
+
+	//name := "User new"
+	//email := "jon133@calhoun.io"
+	//row := db.QueryRow(`
+	//INSERT INTO users (name,email)
+	//VALUES ($1,$2) RETURNING id;`, name, email)
+	//var id int
+	//id := 5
+	//row := db.QueryRow(`
+	//SELECT name, email
+	//FROM users
+	//WHERE id=$1;`, id)
+	//var name, email string
+	//err = row.Scan(&name, &email)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Printf("User information: name %s, email %s\n", name, email)
+	//
+	//userID := 1
+	//for i := 1; i <= 5; i++ {
+	//	amount := i * 100
+	//	desc := fmt.Sprintf("Fake order #%d", i)
+	//	_, err := db.Exec(`
+	//		INSERT INTO orders(user_id,amount,description)
+	//		VALUES($1,$2,$3)`, userID, amount, desc)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}
+	//fmt.Println("Created fake orders.")
+
+	//type Order struct {
+	//	ID          int
+	//	UserID      int
+	//	Amount      int
+	//	Description string
+	//}
+	//
+	//var orders []Order
+	//userID := 1
+	//rows, err := db.Query(`
+	//	SELECT id, amount, description
+	//	FROM orders
+	//	WHERE user_id=$1`, userID)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer rows.Close()
+	//
+	//for rows.Next() {
+	//	var order Order
+	//	order.UserID = userID
+	//	err := rows.Scan(&order.ID, &order.Amount, &order.Description)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//	orders = append(orders, order)
+	//}
+	//err = rows.Err()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println("Orders:", orders)
 }
